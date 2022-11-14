e6c1ceef5fed352a9bb4504ba6749ddd2094d997
Marcin Kunikowski <marcinkunikowski@gmail.com>
2022-11-14 04:03:24 +0100

    Propers solution

--- sum_by_factors/sum_by_factors.py
+++ sum_by_factors/sum_by_factors.py
@@ -1,56 +1,46 @@
 import math
 import numpy as np
 
-x = np.array([[1, 2, 3], [4, 5, 6]], np.int32)
+
+def primes_in_list(x):
+    n = max(x, key=abs)
+    primes = np.arange(3, n + 1, 2)
+    isprime = np.ones((n - 1) // 2, dtype=bool)
+    for factor in primes[:int(math.sqrt(n)) // 2]:
+        if isprime[(factor - 2) // 2]:
+            isprime[(factor * 3 - 2) // 2::factor] = 0
+    return np.insert(primes[isprime], 0, 2)
 
 
-def is_prime(n):
-    if n <= 1:
-        return False
-    max_div = math.floor(math.sqrt(n))
-    for i in range(2, 1 + max_div):
-        if n % i == 0:
-            return False
-    return True
+def sum_for_list(numbers):
+    result = []
+    y = np.array(lst)
 
-
-def my_generator(lst_to_check):
-    primes_in_range = [num for num in range(2, max([abs(num) for num in lst_to_check]) + 1) if is_prime(num)]
-    all_possibilities = np.array([prime, num] for prime in primes_in_range for num in lst_to_check if num % prime == 0)
-    all_possibilities2 = map(lambda x: [x[0], x[1]] if x[1] % x[0] == 0 and x[1] != 0 else None,
-                             ([prime, num] for prime in primes_in_range for num in lst_to_check if num % prime == 0))
-    for j in all_possibilities:
-        if j is not None:
-            yield j
-
-
-def sum_for_list(lst_to_check):
-    result, previous_prime = [], 0
-    all_results = my_generator(lst_to_check)
-
-    for j in all_results:
-        if not result:
-            result.append(j)
-        elif previous_prime != j[0]:
-            result.append(j)
-        else:
-            result[-1][1] += j[1]
-        previous_prime = j[0]
+    for i in primes_in_list(numbers):
+        z = np.sum(y[y % i == 0])
+        if z != 0: result.append([i, abs(z)])
+    print(result)
     return result
 
 
-def profiling(lst_to_time):
+"""def profiling(lst_to_time, max_num):
     import cProfile
     import pstats
 
     with cProfile.Profile() as pr:
-        sum_for_list(lst_to_time)
+        sum_of_numbers(lst_to_time, max_num)
+
     stats = pstats.Stats(pr)
     stats.sort_stats(pstats.SortKey.TIME)
     stats.print_stats()
-    stats.dump_stats(filename="obliczenia.prof")
+    stats.dump_stats(filename="obliczenia.prof")"""
 
+"""def sum_for_list(lst):
+    max_in_lst = max(lst, key=abs)
+    sum_of_numbers(lst, max_in_lst)"""
 
-lst = [x for x in range(30000)]
-profiling(lst)
-# sum_for_list(lst)
+# lst = [x for x in range(100001)]
+
+lst = [15, 21, 24, 30, 45, 150, 121341]
+sum_for_list(lst)
+# profiling(lst, max_in_lst)
